a5efd33af3bc4d15ddc15f2f174abb87f2b158dd
James Nugent <james@jen20.com>
2016-06-13 11:52:43 +0200

    A second fix

--- example_vpc.tf
+++ example_vpc.tf
@@ -35,6 +35,13 @@ resource "aws_security_group" "example" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_main_route_table_association" "example" {
