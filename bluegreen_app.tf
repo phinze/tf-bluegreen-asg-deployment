cf8c61292292dca10d2feeee3514a8623be3c026
James Nugent <james@jen20.com>
2016-06-13 11:36:56 +0200

    Fix up example

--- bluegreen_app.tf
+++ bluegreen_app.tf
@@ -1,36 +1,38 @@
 module "ami" {
-  source = "github.com/terraform-community-modules/tf_aws_ubuntu_ami/ebs"
-  region = "${var.region}"
-  distribution = "trusty"
+  source        = "github.com/terraform-community-modules/tf_aws_ubuntu_ami/ebs"
+  region        = "${var.region}"
+  distribution  = "trusty"
   instance_type = "${var.instance_type}"
 }
 
 resource "aws_elb" "example" {
-  name = "terraform-asg-deployment-example"
-  subnets = ["${aws_subnet.example.*.id}"]
-  security_groups = ["${aws_security_group.example.id}"]
+  name                      = "terraform-asg-deployment-example"
+  subnets                   = ["${aws_subnet.example.*.id}"]
+  security_groups           = ["${aws_security_group.example.id}"]
   cross_zone_load_balancing = true
+
   listener {
-    instance_port = 80
+    instance_port     = 80
     instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
   }
+
   health_check {
-    healthy_threshold = 2
+    healthy_threshold   = 2
     unhealthy_threshold = 2
-    timeout = 2
-    target = "HTTP:80/"
-    interval = 5
+    timeout             = 2
+    target              = "HTTP:80/"
+    interval            = 5
   }
 }
 
 resource "aws_launch_configuration" "blue" {
-  image_id = "${module.ami.ami_id}"
-  instance_type = "${var.instance_type}"
-  key_name = "tftest"
+  image_id        = "${module.ami.ami_id}"
+  instance_type   = "${var.instance_type}"
+  key_name        = "${aws_key_pair.test.key_name}"
   security_groups = ["${aws_security_group.example.id}"]
-  user_data = "${file("./boot-blue.sh")}"
+  user_data       = "${file("./boot-blue.sh")}"
 
   lifecycle {
     create_before_destroy = true
@@ -38,21 +40,21 @@ resource "aws_launch_configuration" "blue" {
 }
 
 resource "aws_autoscaling_group" "blue" {
-  name = "blue"
+  name                 = "blue"
   launch_configuration = "${aws_launch_configuration.blue.id}"
-  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  vpc_zone_identifier = ["${aws_subnet.example.*.id}"]
-  load_balancers = ["${aws_elb.example.name}"]
-  max_size = 0
-  min_size = 0
+  availability_zones   = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  vpc_zone_identifier  = ["${aws_subnet.example.*.id}"]
+  load_balancers       = ["${aws_elb.example.name}"]
+  max_size             = 0
+  min_size             = 0
 }
 
 resource "aws_launch_configuration" "green" {
-  image_id = "${module.ami.ami_id}"
-  instance_type = "${var.instance_type}"
-  key_name = "tftest"
+  image_id        = "${module.ami.ami_id}"
+  instance_type   = "${var.instance_type}"
+  key_name        = "${aws_key_pair.test.key_name}"
   security_groups = ["${aws_security_group.example.id}"]
-  user_data = "${file("./boot-green.sh")}"
+  user_data       = "${file("./boot-green.sh")}"
 
   lifecycle {
     create_before_destroy = true
@@ -60,13 +62,13 @@ resource "aws_launch_configuration" "green" {
 }
 
 resource "aws_autoscaling_group" "green" {
-  name = "green"
+  name                 = "green"
   launch_configuration = "${aws_launch_configuration.green.id}"
-  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  vpc_zone_identifier = ["${aws_subnet.example.*.id}"]
-  load_balancers = ["${aws_elb.example.name}"]
-  max_size = 2
-  min_size = 2
+  availability_zones   = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  vpc_zone_identifier  = ["${aws_subnet.example.*.id}"]
+  load_balancers       = ["${aws_elb.example.name}"]
+  max_size             = 2
+  min_size             = 2
 }
 
 output "dns_name" {
--- example_vpc.tf
+++ example_vpc.tf
@@ -1,3 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_key_pair" "test" {
+  key_name   = "tftest"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
 resource "aws_vpc" "example" {
   cidr_block = "${var.vpc_cidr}"
 }
@@ -8,35 +17,35 @@ resource "aws_internet_gateway" "example" {
 
 resource "aws_route_table" "example" {
   vpc_id = "${aws_vpc.example.id}"
+
   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.example.id}"
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.example.id}"
   }
 }
 
 resource "aws_security_group" "example" {
-  name = "example_sg_allow_all"
+  name        = "example_sg_allow_all"
   description = "example_sg_allow_all"
-  vpc_id = "${aws_vpc.example.id}"
+  vpc_id      = "${aws_vpc.example.id}"
 
   ingress {
-      from_port = 0
-      to_port = 65535
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_main_route_table_association" "example" {
-    vpc_id = "${aws_vpc.example.id}"
-    route_table_id = "${aws_route_table.example.id}"
+  vpc_id         = "${aws_vpc.example.id}"
+  route_table_id = "${aws_route_table.example.id}"
 }
 
 resource "aws_subnet" "example" {
-    vpc_id = "${aws_vpc.example.id}"
-    count = 3
-    cidr_block = "${lookup(var.subnets, count.index)}"
-    availability_zone = "${lookup(var.azs, count.index)}"
-    map_public_ip_on_launch = true
+  vpc_id                  = "${aws_vpc.example.id}"
+  count                   = 3
+  cidr_block              = "${lookup(var.subnets, count.index)}"
+  availability_zone       = "${lookup(var.azs, count.index)}"
+  map_public_ip_on_launch = true
 }
-
